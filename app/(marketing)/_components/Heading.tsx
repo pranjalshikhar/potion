14cde7f635b3f17d3d5c7c56acb7e0b64b47f60b
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2024-01-01 18:08:28 +0530

    established authentication through Clerk

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl md:text-6xl sm:text-5xl font-bold">
@@ -14,10 +20,25 @@ const Heading = () => {
         Potion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Potion
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Potion <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Potion Free <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,8 +4,14 @@ import { useScrollTop } from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +23,20 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <SignInButton mode="modal">
+            <Button>Get Potion Free</Button>
+          </SignInButton>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm">
+              <Link href="/documents">Enter Potion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
