05a201e3dcbf01e9fe089367a481cfe7bea91dd5
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2024-01-01 13:50:19 +0530

    Added Navbar and Dark Mode

---
+++ hooks/useScrollTop.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [threshold]);
+
+  return scrolled;
+};
