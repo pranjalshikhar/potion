1f8146ca4934aee32226f2c1bad14755cf714d6c
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2024-02-18 00:55:41 +0530

    (fix)

---
+++ app/(marketing)/_components/heros.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const Heros = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] md:w-[400px] sm:h-[350px]">
+          <Image
+            src="/illustration1.png"
+            fill
+            alt="Documents"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            alt="Documents"
+            className="object-contain hidden dark:block"
+          />
+        </div>
+        <div className="relative w-[400px] h-[400px] hidden md:block">
+          <Image
+            src="/illustration2.png"
+            fill
+            alt="Reading"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            alt="Reading"
+            className="object-contain hidden dark:block"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heros;
